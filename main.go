d6866e592fcb927d40b7dad3f30247bf5a27b330
kdrag0n <dragon@khronodragon.com>
2018-07-08 18:46:19 -0700

    Split events into seperate file

--- main.go
+++ main.go
@@ -3,62 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
-	"unsafe"
 )
 
 func check(e error) {
 	if e != nil {
 		panic(e)
-	}
-}
-
-func DecodeEvent(r RawEvent) (ev Event) {
-	ev.Time = time.Unix(r.Seconds, r.Microseconds*1000)
-
-	switch r.Type {
-	case RawEvKey:
-		switch r.Code {
-		case RawBtnTouch:
-			if r.Value == 1 {
-				ev.FingerDown = true
-				ev.Type = EvFingerDown
-			} else {
-				ev.FingerDown = false
-				ev.Type = EvFingerUp
-			}
-		}
-
-	case RawEvAbs:
-		switch r.Code {
-		case RawAbsMtSlot:
-			ev.Finger = r.Value
-		case RawAbsMtPositionX:
-			ev.X = r.Value
-		case RawAbsMtPositionY:
-			ev.Y = r.Value
-		}
-
-		if ev.Type == 0 {
-			ev.Type = EvFingerMove
-		}
-	}
-
-	return
-}
-
-func ReadEvents(f *os.File, ch chan Event) {
-	buf := make([]byte, RawEventSize)
-
-	for {
-		_, err := f.Read(buf)
-		check(err)
-
-		bufDataPtr := *(*uintptr)(unsafe.Pointer(&buf))
-		rawEvent := *(*RawEvent)(unsafe.Pointer(bufDataPtr))
-		event := DecodeEvent(rawEvent)
-
-		ch <- event
 	}
 }
 
