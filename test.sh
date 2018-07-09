48cda5198cf226c19e8ee246b4e1499d2895194a
kdrag0n <dragon@khronodragon.com>
2018-07-08 20:56:04 -0700

    Fix testing script

--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" && upx -q nanotap && adb push nanotap /data/local/tmp && adb shell chmod 755 /data/loca
-l/tmp && adb shell su -c /data/local/tmp/nanotap
+GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" && upx -q nanotap > /dev/null 2>&1 && adb push nanotap /data/local/tmp && adb shell chmod 755 /data/local/tmp && adb shell su -c /data/local/tmp/nanotap
 
