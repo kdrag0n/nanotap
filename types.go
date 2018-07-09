5b934501d3300e68520123e1c81b8defe6128d96
kdrag0n <dragon@khronodragon.com>
2018-07-08 18:19:51 -0700

    Add decoding and reading of input events

---
+++ types.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"time"
+	"unsafe"
+)
+
+type RawEvent struct {
+	Seconds      int64
+	Microseconds int64
+	Type         uint16
+	Code         uint16
+	Value        uint32
+}
+
+var _sREvent RawEvent
+
+const RawEventSize = unsafe.Sizeof(_sREvent)
+
+const (
+	RawEvKey           = 1
+	RawEvAbs           = 3
+	RawBtnTouch        = 330
+	RawBtnToolFinger   = 325
+	RawAbsMtTouchMajor = 48
+	RawAbsMtTouchMinor = 49
+	RawAbsMtPositionX  = 53
+	RawAbsMtPositionY  = 54
+	RawAbsMtTrackingID = 57
+	RawAbsMtSlot       = 47
+)
+
+type Event struct {
+	Finger     uint32
+	Type       uint16
+	Time       time.Time
+	FingerDown bool
+	X          uint32
+	Y          uint32
+}
+
+const (
+	EvFingerDown uint16 = iota
+	EvFingerUp
+	EvFingerMove
+)
