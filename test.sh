f568c97e632f6feca746ae39f521eb93fa712a5f
kdrag0n <dragon@khronodragon.com>
2018-07-10 21:31:09 -0700

    test: build with UPX again (fixed)
    Use goupx to fix the binary before running UPX. This doesn't seem
    to have any negative effects at least, so try using it for now.

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" && adb push nanotap config.toml test.stage2.sh /data/local/tmp > /dev/null && adb shell chmod 755 /data/local/tmp/test.stage2.sh && adb shell su -c /data/local/tmp/test.stage2.sh
+GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" && goupx nanotap > /dev/null 2>&1 && adb push nanotap config.toml test.stage2.sh /data/local/tmp > /dev/null && adb shell chmod 755 /data/local/tmp/test.stage2.sh && adb shell su -c /data/local/tmp/test.stage2.sh
+
